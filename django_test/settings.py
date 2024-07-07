50a6a09a33e984dac9ec90839dc100e7ef4b0467
Nikita <nikturchinovich@mail.ru>
2024-07-07 16:19:32 +0300

    Added static files options

--- django_test/settings.py
+++ django_test/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_DIRS = BASE_DIR / 'static'
 STORAGES = {
     "staticfiles": {
