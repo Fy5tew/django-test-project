1b094932a5c4979f6c007ca0b592ec418aa54091
Nikita <nikturchinovich@mail.ru>
2024-07-07 19:04:26 +0300

    Added static files options

--- django_test/settings.py
+++ django_test/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_test.urls'
@@ -122,6 +123,7 @@ STATIC_ROOT = BASE_DIR / 'build' / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
