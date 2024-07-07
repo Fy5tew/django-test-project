8589793b5f51da240d97495200b69d36b9d3d1e5
Nikita <nikturchinovich@mail.ru>
2024-07-07 16:46:11 +0300

    Added static files options

--- django_test/settings.py
+++ django_test/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'staticfiles'
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
